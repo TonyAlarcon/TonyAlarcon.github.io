28fa0fc920aa5b17111df394add4ce513d540eed
Pedro Alarcon <palarcon@g.ucla.edu>
2020-06-05 21:37:33 -0700

    Update mail.php

--- contact/mail.php
+++ contact/mail.php
@@ -3,7 +3,7 @@ $name = $_POST['Name'];
 $email = $_POST['Email'];
 $message = $_POST['Message'];
 $formcontent="From: $Name \n Message: $Message";
-$recipient = "palarcon91@outlook.com";
+$recipient = "alarcon_pedro_antoni@student.smc.edu";
 $subject = "Contact Form";
 $mailheader = "From: $Email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
