d008eab9cb775afa78ca5993c751363b565ab015
Pedro Alarcon <palarcon@g.ucla.edu>
2020-06-05 22:40:28 -0700

    Update mail.php

--- contact/mail.php
+++ contact/mail.php
@@ -1,13 +1,12 @@
 <!-- 
-<?php 
-$name = $_POST['Name'];
-$email = $_POST['Email'];
-$message = $_POST['Message'];
-$formcontent="From: $Name \n Message: $Message";
-$recipient = "alarcon_pedro_antoni@student.smc.edu";
-$subject = "Contact Form";
-$mailheader = "From: $Email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
-?> 
--->
+//<?php 
+//$name = $_POST['Name'];
+//$email = $_POST['Email'];
+//$message = $_POST['Message'];
+//$formcontent="From: $Name \n Message: $Message";
+//$recipient = "alarcon_pedro_antoni@student.smc.edu";
+//$subject = "Contact Form";
+//$mailheader = "From: $Email \r\n";
+//mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+//echo "Thank You!";
+//-->
